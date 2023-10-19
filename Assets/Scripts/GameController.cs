b7e41b9ca54545acd035ccd85d855473fcea22e4
Stegosaurus1117 <122121405+Stegosaurus1117@users.noreply.github.com>
2023-10-19 17:54:32 -0400

    edited my game

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    //Game Over Canvas that is used for the game
+    [Header("Game")]
     // Start is called before the first frame update
     void Start()
     {
